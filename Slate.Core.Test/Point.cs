214ce46d8365693eacecdd822750c1d8a84994e1
felixwatts <felixwatts@gmail.com>
2018-03-17 13:40:45 +0100

    first commit

---
+++ Slate.Core.Test/Point.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+
+namespace Slate.Core.Test
+{
+    public class PointTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(0, 0, 0, 0, 0, 0)]        
+        [TestCase(0, 0, 1, 0, 1, 0)]
+        [TestCase(0, 0, 0, 1, 0, 1)]
+        [TestCase(1, 0, 0, 0, 1, 0)]
+        [TestCase(0, 1, 0, 0, 0, 1)]
+        [TestCase(1, 0, 1, 0, 2, 0)]
+        [TestCase(0, 1, 0, 1, 0, 2)]
+        [TestCase(1, 0, 0, 1, 1, 1)]
+        [TestCase(1, 0, -1, 0, 0, 0)]
+        [TestCase(-1, 0, 1, 0, 0, 0)]
+        [TestCase(-1, 0, -1, 0, -2, 0)]
+        [TestCase(0, 1, 0, -1, 0, 0)]
+        [TestCase(0, -1, 0, 1, 0, 0)]
+        [TestCase(0, -1, 0, -1, 0, -2)]
+        public void Add(long xLeft, long yLeft, long xRight, long yRight, long expectedX, long expectedY)
+        {
+            var left = new Point(xLeft, yLeft);
+            var right = new Point(xRight, yRight);
+
+            var actual = left + right;
+
+            Assert.AreEqual(expectedX, actual.X);
+            Assert.AreEqual(expectedY, actual.Y);
+        }
+
+        [TestCase(0, 0, 0, 0, 0, 0)]        
+        [TestCase(0, 0, 1, 0, -1, 0)]
+        [TestCase(0, 0, 0, 1, 0, -1)]
+        [TestCase(1, 0, 0, 0, -1, 0)]
+        [TestCase(0, 1, 0, 0, 0, -1)]
+        [TestCase(1, 0, 1, 0, -2, 0)]
+        [TestCase(0, 1, 0, 1, 0, -2)]
+        [TestCase(1, 0, 0, 1, -1, -1)]
+        [TestCase(1, 0, -1, 0, 0, 0)]
+        [TestCase(-1, 0, 1, 0, 0, 0)]
+        [TestCase(-1, 0, -1, 0, 2, 0)]
+        [TestCase(0, 1, 0, -1, 0, 0)]
+        [TestCase(0, -1, 0, 1, 0, 0)]
+        [TestCase(0, -1, 0, -1, 0, 2)]
+        public void Subtract(long xLeft, long yLeft, long xRight, long yRight, long expectedX, long expectedY)
+        {
+            var left = new Point(xLeft, yLeft);
+            var right = new Point(xRight, yRight);
+
+            var actual = left - right;
+
+            Assert.AreEqual(expectedX, actual.X);
+            Assert.AreEqual(expectedY, actual.Y);
+        }
+    }
+}
---
+++ Slate.Core/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Slate.Core
+{
+    public class Point : IEquatable<Point>
+    {
+        public long X { get; }
+        public long Y { get; }
+
+        public Point(long x, long y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Point operator +(Point left, Point right)
+        {
+            return new Point(left.X + right.X, left.Y + right.Y);
+        }
+
+        public static Point operator -(Point left, Point right)
+        {
+            return new Point(left.X - right.X, left.Y - right.Y);
+        }
+
+        public static Point operator -(Point point)
+        {
+            return new Point(-point.X, -point.Y);
+        }
+
+        public bool Equals(Point other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + X.GetHashCode();
+                hash = hash * 31 + Y.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
