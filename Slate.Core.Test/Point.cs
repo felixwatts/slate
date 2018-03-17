909213100079c65f68f191652c90b7246c692254
felixwatts <felixwatts@gmail.com>
2018-03-17 14:04:07 +0100

    added tests for Point negate

--- Slate.Core.Test/Point.cs
+++ Slate.Core.Test/Point.cs
@@ -40,12 +40,12 @@ namespace Slate.Core.Test
         [TestCase(1, 0, 0, 0, 1, 0)]
         [TestCase(0, 1, 0, 0, 0, 1)]
         [TestCase(1, 0, 1, 0, 0, 0)]
-        [TestCase(0, 1, 0, 1, 0, -2)]
+        [TestCase(0, 1, 0, 1, 0, 0)]
         [TestCase(1, 0, 0, 1, 1, -1)]
         [TestCase(1, 0, -1, 0, 2, 0)]
         [TestCase(-1, 0, 1, 0, -2, 0)]
         [TestCase(-1, 0, -1, 0, 0, 0)]
-        [TestCase(0, 1, 0, -1, 0, 0)]
+        [TestCase(0, 1, 0, -1, 0, 2)]
         [TestCase(0, -1, 0, 1, 0, -2)]
         [TestCase(0, -1, 0, -1, 0, 0)]
         public void Subtract(long xLeft, long yLeft, long xRight, long yRight, long expectedX, long expectedY)
@@ -54,6 +54,21 @@ namespace Slate.Core.Test
             var right = new Point(xRight, yRight);
 
             var actual = left - right;
+
+            Assert.AreEqual(expectedX, actual.X);
+            Assert.AreEqual(expectedY, actual.Y);
+        }
+
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(1, 0, -1, 0)]
+        [TestCase(0, 1, 0, -1)]
+        [TestCase(1, 1, -1, -1)]
+        [TestCase(-1, -1, 1, 1)]
+        public void Negate(long x, long y, long expectedX, long expectedY)
+        {
+            var point = new Point(x, y);
+
+            var actual = - point;
 
             Assert.AreEqual(expectedX, actual.X);
             Assert.AreEqual(expectedY, actual.Y);
